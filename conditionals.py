632081b994f7e8d5fa3f03ea918a111e4d582232
Aranda <ajabiro1211@gmail.com>
2023-10-05 04:15:55 -0400

    initial commit

---
+++ conditionals.py
@@ -0,0 +1,16 @@
+userNumber = int(input("Enter an integer between 1 and 100: "))
+
+if userNumber % 2 == 0:
+    if 2 <= userNumber <= 24:
+        print(f"{userNumber} is even and less than 25")
+    elif 26 <= userNumber <= 60:
+        print(f"{userNumber} is even and between 26 and 60 inclusive")
+    elif userNumber > 60:
+        print(f"{userNumber} is even and greater than 60")
+elif userNumber % 2 == 1:
+    if userNumber < 60:
+        print(f"{userNumber} is odd and less than 60")
+    elif userNumber > 60:
+        print(f"{userNumber} is odd and greater than 60")
+# elif userNumber > 100:
+#     print("Must enter number 1 - 100")
